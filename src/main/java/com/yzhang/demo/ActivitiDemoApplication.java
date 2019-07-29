c887a137b44ddf72c1161f85fab6a9139688c3fd
yzhang <yi_zhang2@intuit.com>
2019-07-29 10:37:28 -0700

    fix

--- src/main/java/com/yzhang/demo/ActivitiDemoApplication.java
+++ src/main/java/com/yzhang/demo/ActivitiDemoApplication.java
@@ -9,17 +9,6 @@ import javax.sql.DataSource;
 
 @SpringBootApplication
 public class ActivitiDemoApplication {
-
-    @Bean
-    public DataSource database() {
-        return DataSourceBuilder.create()
-                .url("jdbc:mysql://127.0.0.1:3306/activiti?serverTimezone=UTC&characterEncoding=UTF-8")
-                .username("root")
-                .password("yuma2017!")
-                .driverClassName("com.mysql.jdbc.Driver")
-                .build();
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(ActivitiDemoApplication.class, args);
     }
